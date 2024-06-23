9e1077b5b004353cbbae4fc1e2741ca368e74016
Joaquim <oliver-junior@hotmail.com>
2024-06-23 20:43:00 -0300

    Update a_trainner.py
    Trainner class 66

--- a_trainner.py
+++ a_trainner.py
@@ -1,8 +1,7 @@
-def name(name):
-    print(f'{name} Oliveira')
+def soma(x, y, z):
+    print(x + y + z)
 
-name('Joaquim')
-name('Alyne')
-name('Joao Pedro')
-name('Mariane Vitória')
-
+soma(1,2,3)
+soma(5,25,10)
+soma(7,9,30)
+soma(10,20,30)
