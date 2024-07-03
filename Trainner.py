ea6a399a177ef95b390e34e8ce02728ec0b91003
Joaquim <oliver-junior@hotmail.com>
2024-07-03 16:17:02 -0300

    class 120

--- Trainner.py
+++ Trainner.py
@@ -1,8 +1,16 @@
-import hello
-import sys
-#125, 132, 137
+produtos = [
+    {'nome': 'Produto 5', 'preco': 10.00},
+    {'nome': 'Produto 1', 'preco': 22.32},
+    {'nome': 'Produto 3', 'preco': 10.11},
+    {'nome': 'Produto 2', 'preco': 105.87},
+    {'nome': 'Produto 4', 'preco': 69.90},
+]
 
-print(hello.mult())
-print(sys)
+novo_produto = [
+    {**produto, 'preco': round(produto['preco']*1.1,2)}
+    for produto in produtos
+]
+
+print(novo_produto)
 
 
