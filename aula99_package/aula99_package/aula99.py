112f69f6c98097b4c36412b34314ba24dc2ea2a7
Joaquim <oliver-junior@hotmail.com>
2024-07-03 16:39:05 -0300

    class 120

--- aula99_package/aula99_package/aula99.py
+++ aula99_package/aula99_package/aula99.py
@@ -2,9 +2,9 @@
 # https://stackoverflow.com/questions/2386714/why-is-import-bad
 
 # import aula99_package.modulo
-# from aula99_package import modulo
-@@ -12,7 +13,12 @@
-# print(modulo.soma_do_modulo(1, 2))
+# # from aula99_package import modulo
+# @@ -12,7 +13,12 @@
+# # print(modulo.soma_do_modulo(1, 2))
 # print(variavel)
 # print(nova_variavel)
 from aula99_package.modulo import fala_oi, soma_do_modulo
