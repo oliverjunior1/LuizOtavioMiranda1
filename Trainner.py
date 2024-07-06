ed759b461db724b988a96c1f7640879258b9df87
Joaquim <oliver-junior@hotmail.com>
2024-07-06 18:11:37 -0300

    Update Trainner.py

--- Trainner.py
+++ Trainner.py
@@ -2,43 +2,38 @@ class Car:
     def __init__(self, name):
         self.name = name
         self._engine = None
-        self._manufacturer = None
+        self._manufaturator= None
 
     @property
     def engine(self):
         return self._engine
     
-    @engine.setter
+    @engine.getter
     def engine(self, value):
         self._engine = value
-
+        
     @property
-    def manufacturer(self):
-        return self._manufacturer
+    def manufaturator(self):
+        return self._manufaturator
     
-    @manufacturer.setter
-    def manufacturer(self, value):
-        self._manufacturer = value
-
-
+    @manufaturator.getter
+    def manufaturator(self, value):
+        self._manufaturator = value
+        
 class Engine:
     def __init__(self, name):
         self.name = name
 
-class Manufacturing:
+class Manufaturator:
     def __init__(self, name):
         self.name = name
 
 fusca = Car('Fusca')
 engine_1_0 = Engine('1.0')
-volkswagen = Manufacturing('Volkswagen')
-fusca.engine = engine_1_0
-fusca.manufacturer = volkswagen
-print(fusca.name, fusca.manufacturer.name, fusca.engine.name)
+volkswagen = Manufaturator('Volkswagen')
+fusca.engine= engine_1_0
+fusca.manufaturator = volkswagen
 
-uno = Car('uno')
-engine_1_0 = Engine('1.0')
-fiat = Manufacturing('fiat')
-uno.engine = engine_1_0
-uno.manufacturer = fiat
-print(uno.name, uno.manufacturer.name, uno.engine.name)
+print(fusca.engine.name, fusca.name, fusca.manufaturator.name)
+
+
